39b6053899e3bec172a7aec314846c3b27fb0a64
Francois Touchard <francois.touchard.wos@gmail.com>
2025-10-17 11:37:59 +0200

    feat(backend): allowing crossOrigin on user controller

--- backend/pmt/src/main/java/com/iscod/pmt/controllers/UtilisateurController.java
+++ backend/pmt/src/main/java/com/iscod/pmt/controllers/UtilisateurController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,6 +22,7 @@ import com.iscod.pmt.models.Utilisateur;
 import com.iscod.pmt.services.UtilisateurService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/users")
 public class UtilisateurController {
 	
