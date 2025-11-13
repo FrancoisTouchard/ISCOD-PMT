24043e4754d581db0ee1c5a214b6ae13b0c0bd6c
Francois Touchard <francois.touchard.wos@gmail.com>
2025-11-13 19:12:56 +0100

    feat(frontend, project modal): uniformizing modals, adding validators

--- frontend/pmt/src/app/components/modals/project-modal/project-modal.component.ts
+++ frontend/pmt/src/app/components/modals/project-modal/project-modal.component.ts
@@ -25,8 +25,11 @@ export class ProjectModalComponent {
   constructor() {
     this.projectCreationForm = new FormGroup({
       projectData: new FormGroup({
-        name: new FormControl(null, [Validators.required]),
-        description: new FormControl(null),
+        name: new FormControl(null, [
+          Validators.required,
+          Validators.maxLength(100),
+        ]),
+        description: new FormControl(null, [Validators.maxLength(250)]),
         startDate: new FormControl(null),
       }),
     });
